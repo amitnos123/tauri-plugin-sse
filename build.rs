552c731aba9278db54b9d90f0abd724868384c7a
sami.nosatzki <sami.nosatzki@gmail.com>
2025-11-03 02:56:38 +0200

    update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
+use std::env;
+use std::path::Path;
+
 const COMMANDS: &[&str] = &["ping"];
 
 fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let permissions_dir = Path::new(&out_dir).join("permissions");
+
+    tauri_plugin::Builder::new(COMMANDS)
+        .android_path("android")
+        .ios_path("ios")
+        .out_dir(permissions_dir) // <-- write generated files here
+        .build();
 }
+
