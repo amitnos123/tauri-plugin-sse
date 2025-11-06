0e79ac51c65c621d1acb050a5e74cef55d2370f1
Amit Sami Nosatzki <42678113+amitnos123@users.noreply.github.com>
2025-11-06 02:41:00 +0200

    Add EventSource connection and event handlers
    Implement connection to server endpoint and handle events.

--- src/commands.rs
+++ src/commands.rs
@@ -11,3 +11,33 @@ pub(crate) async fn ping<R: Runtime>(
 ) -> Result<PingResponse> {
     app.sse().ping(payload)
 }
+
+/*TOOD implemented
+  // Create connection to server endpoint
+let event_source = EventSource::new("http://event-stream-address/sub").unwrap();
+
+// Handle on establishing connection
+event_source.on_open(|| {
+    println!("Connection stabilished!");
+});
+
+// Fired when a message is received
+event_source.on_message(|message| {
+    println!("New message event {:?}", message);
+});
+
+// Handle errors
+event_source.on_error(|error| {
+    println!("Error {:?}", error);
+});
+
+// Handle named event types
+event_source.add_event_listener("myEvent", |event| {
+    println!("Event {} received: {}", event.type_, event.data);
+});
+
+// Remove handler
+event_source.remove_event_listener("myEvent");
+
+// Close Event Source
+event_source.close
