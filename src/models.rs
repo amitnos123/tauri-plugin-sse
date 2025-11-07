a39ba202de0c559b89912863500386fb0c6245e0
Amit Sami Nosatzki <42678113+amitnos123@users.noreply.github.com>
2025-11-07 03:39:15 +0200

    Add EventSource handling code as comments
    Added commented-out code for EventSource handling.

--- src/models.rs
+++ src/models.rs
@@ -11,3 +11,31 @@ pub struct PingRequest {
 pub struct PingResponse {
   pub value: Option<String>,
 }
+
+
+/* TODO make models
+let event_source = EventSource::new("http://event-stream-address/sub").unwrap();
+
+// Handle on establishing connection
+event_source.on_open(|| {
+    println!("Connection stabilished!");
+});
+
+// Fired when a message is received
+event_source.on_message(|message| {
+    println!("New message event {:?}", message);
+});
+
+// Handle errors
+event_source.on_error(|error| {
+    println!("Error {:?}", error);
+});
+
+// Handle named event types
+event_source.add_event_listener("myEvent", |event| {
+    println!("Event {} received: {}", event.type_, event.data);
+});
+
+// Remove handler
+event_source.remove_event_listener("myEvent");
+*/
