f3034bba0b6587f1b718c37079af43fb348b74ae
sami.nosatzki <sami.nosatzki@gmail.com>
2025-11-19 01:08:52 +0200

    implemented commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,4 +1,4 @@
-use tauri::{AppHandle, command, Runtime, async_runtime::Mutex};
+use tauri::{Emitter, Manager, State, AppHandle, command, Runtime, async_runtime::Mutex};
 
 use std::collections::HashMap;
 
@@ -7,6 +7,11 @@ use sse_client::EventSource;
 use crate::models::*;
 use crate::Result;
 use crate::SseExt;
+
+pub(crate) fn event_full_name(url: &str, name: &str) -> String {
+    let event_start_name = "tauri-plugin-sse-";
+    event_start_name.to_string() + url + "-" + name
+}
 
 #[derive(Default)]
 pub struct AppState {
@@ -29,57 +34,115 @@ pub(crate) async fn ping<R: Runtime>(
 #[command]
 pub(crate) async fn open_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: OpenRequest,
-) -> Result<OpenResponse> {
-    let state = app.state::<Mutex<AppState>>();
+    url: String,
+) -> Result<bool> {
+    let state: tauri::State<'_, Mutex<AppState>> = app.state::<Mutex<AppState>>();
 
-    let url = "http://event-stream-address/sub".to_string();
-    let event_source = EventSource::new("http://event-stream-address/sub").unwrap();
+    // Create a new EventSource instance for the given URL.
+    let event_source = EventSource::new(&url).unwrap();
 
     // Lock the mutex to mutably access the state.
-    let mut state = state.lock().unwrap();
+    let mut state = state.lock().await;
     state.events.insert(url, event_source);
+
+    Ok(true)
 }
 
 #[command]
-pub(crate) async fn on_message_sse<R: Runtime>(
+pub(crate) async fn add_on_message_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: OnMessageRequest,
-) -> Result<OnMessageResponse> {
+    url: String,
+) -> Result<bool> {
+    let state: tauri::State<'_, Mutex<AppState>> = app.state::<Mutex<AppState>>();
+    let mut state = state.lock().await;
+    
+    let app_clone = app.clone();
+    let event_full_name = event_full_name(&url, "on_message");
+    if let Some(event_source) = state.events.get_mut(&url) {
+        event_source.on_message(move |message| {
+                    let payload = TauriEventSse::from_client_event_sse(message);
+                    app_clone.emit(&event_full_name, payload).unwrap();
+                }
+            );
+        return Ok(true);
+    }
+    Ok(false)
 }
 
 #[command]
-pub(crate) async fn on_error_sse<R: Runtime>(
+pub(crate) async fn add_on_error_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: OnErrorRequest,
-) -> Result<OnErrorResponse> {
+    url: String,
+) -> Result<bool> {
+    let state: tauri::State<'_, Mutex<AppState>> = app.state::<Mutex<AppState>>();
+    let mut state = state.lock().await;
+
+    if let Some(event_source) = state.events.get_mut(&url) {
+        let app_clone = app.clone();
+        let event_full_name = event_full_name(&url, "error");
+        event_source.add_event_listener("error" ,move |event| {
+                    let payload = TauriEventSse::from_client_event_sse(event);
+                    app_clone.emit(&event_full_name, payload).unwrap();
+                }
+            );
+        return Ok(true);
+    }
+    Ok(false)
 }
 
 #[command]
 pub(crate) async fn add_event_listener_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: AddEventListenerRequest,
-) -> Result<AddEventListenerResponse> {
+    url: String,
+    name: String
+) -> Result<bool> {
+    let state: tauri::State<'_, Mutex<AppState>> = app.state::<Mutex<AppState>>();
+    let mut state = state.lock().await;
+
+    if let Some(event_source) = state.events.get_mut(&url) {
+        let app_clone = app.clone();
+        let name_clone = name.clone();
+        let event_full_name = event_full_name(&url, &name_clone);
+        event_source.add_event_listener(&name ,move |event| {
+                    let payload = TauriEventSse::from_client_event_sse(event);
+                    app_clone.emit(&event_full_name, payload).unwrap();
+                }
+            );
+        return Ok(true);
+    }
+    Ok(false)
 }
 
 #[command]
 pub(crate) async fn remove_event_listener_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: RemoveEventListenerRequest,
-) -> Result<RemoveEventListenerResponse> {
+    url: String,
+    name: String
+) -> Result<bool> {
+    let state: tauri::State<'_, Mutex<AppState>> = app.state::<Mutex<AppState>>();
+    let mut state = state.lock().await;
+
+    if let Some(event_source) = state.events.get_mut(&url) {
+        event_source.add_event_listener(&name ,|event| {}); // Do nothing on event
+        return Ok(true);
+    }
+    Ok(false)
 }
 
 #[command]
 pub(crate) async fn close_sse<R: Runtime>(
     app: AppHandle<R>,
-    payload: CloseRequest,
-) -> Result<CloseResponse> {
-    let url = "http://event-stream-address/sub".to_string();
+    url: String,
+) -> Result<bool> {
     let state = app.state::<Mutex<AppState>>();
-    if state.events.contains_key(url) {
-        let event_source = state.get(url);
+    let mut state = state.lock().await;
+
+    // Remove the event source from the map
+    if let Some(event_source) = state.events.remove(&url) {
+        // Now that it's removed, no borrows exist → safe to close
         event_source.close();
-        let mut state = state.lock().unwrap();
-        state.events.remove(url)
+
+        return Ok(true);
     }
+    Ok(false)
 }
--- src/models.rs
+++ src/models.rs
@@ -12,75 +12,16 @@ pub struct PingResponse {
   pub value: Option<String>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OpenRequest {
-  pub value: Option<String>,
-}
-
 #[derive(Debug, Clone, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct OpenResponse {
-  pub value: Option<String>,
+pub struct TauriEventSse {
+    id: String,
+    type_: String,
+    data: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct CloseRequest {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct CloseResponse {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AddEventListenerRequest {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AddEventListenerResponse {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct RemoveEventListenerRequest {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct RemoveEventListenerResponse {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OnMessageRequest {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OnMessageResponse {
-  pub value: Option<String>,
-}
-
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OnErrorRequest {
-  pub value: Option<String>,
-}
-
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct OnErrorResponse {
-  pub value: Option<String>,
+impl TauriEventSse {
+    pub fn from_client_event_sse(client_event_sse: sse_client::Event) -> Self {
+      TauriEventSse { id: client_event_sse.id, type_: client_event_sse.type_, data: client_event_sse.data }
+    }
 }
